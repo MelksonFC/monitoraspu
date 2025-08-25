45b78452495eec022243d074dc2f6e0fa119e559
Melkson Felix <melkson_silva@yahoo.com.br>
2025-08-25 20:05:24 -0300

    commit - configuracoes de deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  base: "/monitoraspu",
   resolve: {
     alias: {
       // Aqui está a mágica: estamos dizendo ao Vite
