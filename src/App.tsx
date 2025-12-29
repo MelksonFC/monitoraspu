fafa9003461695e833788600d6da78159f830b58
Melkson Felix <melkson_silva@yahoo.com.br>
2025-12-29 16:05:30 -0300

    Commit mudar app base para barra

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ const App: React.FC = () => {
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ptBR}>
       <AuthProvider>
-        <BrowserRouter basename="/monitoraspu">
+        <BrowserRouter basename="/">
         <ToastContainer
           position="top-right"
           autoClose={5000}
