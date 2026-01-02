af4db61b0a7052b5f4407733f9f2080eaa139301
Melkson Felix <melkson_silva@yahoo.com.br>
2026-01-02 16:36:48 -0300

    fix: Corrige configuração SSL do Sequelize para conexão com o Render

--- server/models/sequelize.js
+++ server/models/sequelize.js
@@ -3,28 +3,36 @@
 const { Sequelize } = require("sequelize");
 const dbConfig = require("../config/database");
 
+const sequelizeOptions = {
+  ...dbConfig,
+  dialect: 'postgres',
+  pool: {
+    max: 10,
+    min: 2,
+    acquire: 30000,
+    idle: 10000
+  },
+  define: {
+    freezeTableName: true,
+    schema: 'dbo'
+  },
+  dialectOptions: {
+    searchPath: 'dbo,public'
+  }
+};
+
+// O Sequelize espera a configuração SSL dentro de 'dialectOptions'.
+// Se a configuração 'ssl' existir no config principal (para o Render),
+// nós a movemos para o lugar correto para o Sequelize.
+if (sequelizeOptions.ssl) {
+  sequelizeOptions.dialectOptions.ssl = sequelizeOptions.ssl;
+}
+
 const sequelize = new Sequelize(
   dbConfig.database,
   dbConfig.user,
   dbConfig.password,
-  {
-    ...dbConfig,
-    dialect: 'postgres',
-    pool: {
-      max: 10,
-      min: 2,
-      acquire: 30000,
-      idle: 10000
-    },
-    define: {
-      freezeTableName: true,
-      schema: 'dbo'
-    },
-    dialectOptions: {
-      ...dbConfig.dialectOptions,
-      searchPath: 'dbo,public'
-    }
-  }
+  sequelizeOptions
 );
 
 module.exports = sequelize;
