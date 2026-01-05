1eb1a95b9e9753a97b87a200e6eeb76bc24c2e37
Melkson Felix <melkson_silva@yahoo.com.br>
2026-01-05 16:11:48 -0300

    MELHORIA EXPERIENCIA USUARIO MODO ESCURO 12 - Padronizacao cores botoes IconButton

--- sql/add_compactmode_to_usertablesettings.sql
+++ sql/add_compactmode_to_usertablesettings.sql
@@ -1,6 +1,26 @@
 -- Adiciona campos compactmode e rowsperpage à tabela usertablesettings
 -- Execute este script no banco de dados PostgreSQL
 
+CREATE EXTENSION postgis WITH SCHEMA dbo;
+
+-- Criação da tabela dbo.poligonosterreno
+CREATE TABLE dbo.poligonosterreno (
+	id serial4 NOT NULL,
+	idimovel int4 NOT NULL,
+	area dbo.geometry(polygon, 4326) NULL,
+	usermodified int4 NOT NULL,
+	usercreated int4 NOT NULL,
+	datemodified timestamp NOT NULL,
+	datecreated timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	CONSTRAINT poligonosterreno_pkey PRIMARY KEY (id)
+);
+
+
+-- dbo.poligonosterreno chaves estrangeiras
+
+ALTER TABLE dbo.poligonosterreno ADD CONSTRAINT poligonosterreno_idimovel_fkey FOREIGN KEY (idimovel) REFERENCES dbo.imoveis(idimovel);
+;
+
 -- Adiciona campo compactmode
 ALTER TABLE dbo.usertablesettings 
 ADD COLUMN IF NOT EXISTS compactmode BOOLEAN DEFAULT FALSE;
